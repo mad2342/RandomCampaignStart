6d5887ca9b565653f9731ae745f9ecbbbc557c19
Mad <github@typ03.de>
2020-01-30 23:39:51 +0100

    refactor

--- Source/RandomCampaignStart/RandomCampaignStart.cs
+++ Source/RandomCampaignStart/RandomCampaignStart.cs
@@ -9,6 +9,39 @@ using System.Linq;
 
 namespace RandomCampaignStart
 {
+    public static class RandomCampaignStart
+    {
+        internal static string LogPath;
+        internal static string ModDirectory;
+        internal static Settings Settings;
+        // BEN: DebugLevel (0: nothing, 1: error, 2: debug, 3: info)
+        internal static int DebugLevel = 2;
+
+        public static void Init(string directory, string settings)
+        {
+            ModDirectory = directory;
+            LogPath = Path.Combine(ModDirectory, "RandomCampaignStart.log");
+
+            Logger.Initialize(LogPath, DebugLevel, ModDirectory, nameof(RandomCampaignStart));
+
+            try
+            {
+                Settings = JsonConvert.DeserializeObject<Settings>(settings);
+            }
+            catch (Exception e)
+            {
+                Settings = new Settings();
+                Logger.LogError(e);
+            }
+
+            // Harmony calls need to go last here because their Prepare() methods directly check Settings...
+            HarmonyInstance harmony = HarmonyInstance.Create("de.mad.RandomCampaignStart");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+
+
     [HarmonyPatch(typeof(SimGameState), "FirstTimeInitializeDataFromDefs")]
     public static class SimGameState_FirstTimeInitializeDataFromDefs_Patch
     {
@@ -202,7 +235,7 @@ namespace RandomCampaignStart
 
 
 
-    internal class ModSettings
+    internal class Settings
     {
         public List<string> AssaultMechsPossible = new List<string>();
         public List<string> HeavyMechsPossible = new List<string>();
@@ -223,64 +256,5 @@ namespace RandomCampaignStart
         public int NumberRandomRonin = 4;
 
         public bool RemoveAncestralMech = false;
-    }
-
-    public static class RandomCampaignStart
-    {
-        internal static ModSettings Settings;
-        internal static string LogPath;
-        internal static string ModDirectory;
-
-        // BEN: Debug (0: nothing, 1: errors, 2:all)
-        internal static int DebugLevel = 2;
-
-        public static void Init(string directory, string settings)
-        {
-            var harmony = HarmonyInstance.Create("de.mad.RandomCampaignStart");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
-
-            ModDirectory = directory;
-            LogPath = Path.Combine(ModDirectory, "RandomCampaignStart.log");
-            File.CreateText(RandomCampaignStart.LogPath);
-
-            // read settings
-            try
-            {
-                Settings = JsonConvert.DeserializeObject<ModSettings>(settings);
-            }
-            catch (Exception)
-            {
-                Settings = new ModSettings();
-            }
-        }
-    }
-
-    public class Logger
-    {
-        static string filePath = RandomCampaignStart.LogPath;
-        public static void LogError(Exception ex)
-        {
-            if (RandomCampaignStart.DebugLevel >= 1)
-            {
-                using (StreamWriter writer = new StreamWriter(filePath, true))
-                {
-                    var prefix = "[RandomCampaignStart @ " + DateTime.Now.ToString() + "]";
-                    writer.WriteLine("Message: " + ex.Message + "<br/>" + Environment.NewLine + "StackTrace: " + ex.StackTrace + "" + Environment.NewLine);
-                    writer.WriteLine("----------------------------------------------------------------------------------------------------" + Environment.NewLine);
-                }
-            }
-        }
-
-        public static void LogLine(String line)
-        {
-            if (RandomCampaignStart.DebugLevel >= 2)
-            {
-                using (StreamWriter writer = new StreamWriter(filePath, true))
-                {
-                    var prefix = "[RandomCampaignStart @ " + DateTime.Now.ToString() + "]";
-                    writer.WriteLine(prefix + line);
-                }
-            }
-        }
     }
 }
